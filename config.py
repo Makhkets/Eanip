8949ff017a990d7d751ba1a72517444a13206b74
Benefix <89061583+Benefixx@users.noreply.github.com>
2022-05-15 01:28:48 +0300

    add blog section

---
+++ config.py
@@ -0,0 +1,45 @@
+"""
+https://i.ibb.co/cyvJdzM/product5.png - портфель
+https://i.ibb.co/1RXXX8n/product1.png - часы
+https://i.ibb.co/gtHz4Kq/product2.png - крем
+https://i.ibb.co/NWvv6QJ/product3.png - лампа
+"""
+
+token = "555bf742d6e4f7014665dc943543ea08"
+secret_key = "eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6InVtZWs3NC0wMCIsInVzZXJfaWQiOiI3OTM4ODk1NDI1MCIsInNlY3JldCI6ImQ1N2RhNjM1MjVmYWFhODYwNWY3NGFjYzI2MzkxMzRjYTU3YjVlMDlkMTIwNjdlYTU4NGQyZDM4MjFkNzU2NTEifX0="
+number = "79388954250"
+nickname = "WHITERESELLER"
+TELEGRAM_BOT_TOKEN = "5123458549:AAHtYHPnWSj_GMFMbWrOQ56UEqFY6ssZ6Wo"
+
+categories = {
+                                                "a" : "Транспорт",
+                                                "b" : "Работа",
+                                                "c" : "Акссесуары",
+                                                "d" : "Для дома и дачи",
+                                                "e" : "Недвижимость",
+                                                "f" : "Услуги",
+                                                "g" : "Электроника",
+                                                "h" : "Животные",
+                                                "i" : "Готовый бизнес",
+                                                "j" : "Оборудование"
+}
+
+colors = {
+
+                                                "a" : "Черный",
+                                                "b" : "Белый",
+                                                "c" : "Красный",
+                                                "d" : "Зеленый",
+                                                "e" : "Синий",
+                                                "f" : "Оранжевый",
+                                                "g" : "Фиолетовый",
+                                                "h" : "Серый",
+                                                "i" : "Голубой"
+}
+
+conditions = {
+
+                                                "a" : "Идеальное состояние",
+                                                "b" : "Б / У",
+                                                "c" : "Немного хуже"
+}
